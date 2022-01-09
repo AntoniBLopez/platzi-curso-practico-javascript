a3956aef14df15293358a9b11afc28006098e6e2
Toni B.Lopez local <tonibassols@outlook.es>
2022-01-09 13:08:26 +0100

    Figuras geométricas pasadas a arrow functions

--- figuras.js
+++ figuras.js
@@ -1,63 +1,63 @@
-// Código de un Cuadrado
+// Código de un Cuadrado con arrow functions
 
-let ladoC = 5;
-let perimetroC = ladoC * 4;
-let areaC = ladoC * ladoC;
+let perimetroC = (lado)=> lado * 4;
+
+let areaC = (lado)=> lado * lado;
+
 
 document.write(
     `<strong>El lado del Cuadrado mide:</strong>
-    <br>${ladoC}cm<p>
+    <br>${5}cm<p>
     <strong>El perímetro es de:</strong>
-    <br>${perimetroC}cm<p>
+    <br>${perimetroC(5)}cm<p>
     <strong>El área és de:</strong>
-    <br>${areaC}cm²<p>`
+    <br>${areaC(5)}cm²<p>`
 );
 
-// Código de un Triángulo
+// Código de un Triángulo con arrow functions
 
-let ladoT1 = 6;
-let ladoT2 = 6;
-let baseT = 4;
-let alturaT = 5.5;
-let parametroT = ladoT1 + ladoT2 + baseT;
-let areaT = baseT * alturaT / 2;
+
+let perimetroT = (ladoT1, ladoT2, baseT)=> ladoT1 + ladoT2 + baseT;
+
+let areaT = (baseT, alturaT)=> baseT * alturaT / 2;
 
 document.write(
     `<strong>Los lados del Triángulo miden:</strong>
-    <br>${ladoT1}cm<br>${ladoT2}cm<br>${baseT}cm<p>
+    <br>${6}cm<br>${6}cm<br>${4}cm<p>
     <strong>La altura es de:</strong>
-    <br>${alturaT}<p>
+    <br>${5.5}<p>
     <strong>El parámetro es de:</strong>
-    <br>${parametroT}<p>
+    <br>${perimetroT(6, 6, 4)}<p>
     <strong>La área es de:</strong>
-    <br>${areaT}<p>`
+    <br>${areaT(4, 5.5)}<p>`
 );
 
-// Código de un Círculo
+// Código de un Círculo con arrow functions
 
 
 // Necesito el rádio
-let radioCirculo = 4;
+
+// El rádio por defecto es de: let radioCirculo = 4;
 
 // El diámetro
-let diametroCirculo = radioCirculo * 2;
+let diametroCirculo = (radioCirculo)=> radioCirculo * 2;
 
 // El número π
 let PI = Math.PI;
 
 // La circunferencia
-let perimetroCirculo = diametroCirculo * PI;
+let perimetroCirculo = diametroCirculo(4) * PI;
 
 // El área
-let areaCirculo = radioCirculo * radioCirculo * PI;
+let areaCirculo = (radioCirculo)=> radioCirculo * radioCirculo * PI;
 
 document.write(
     `<strong>El Rádio del Círculo es de:</strong>
-    <br>${radioCirculo}<p>
+    <br>${4}<p>
     <strong>El diámetro es de:</strong>
-    <br>${diametroCirculo}<p>
+    <br>${diametroCirculo(4)}<p>
     <strong>La Circunferencia es de:</strong>
     <br>${perimetroCirculo}<p>
     <strong>El área es de:</strong>
-    <br>${areaCirculo}`
+    <br>${areaCirculo(4)}`
 );
