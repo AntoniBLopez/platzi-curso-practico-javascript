0aadccd5f37d31815e22be9256efdc876d63faed
Toni B.Lopez local <tonibassols@outlook.es>
2022-01-10 12:39:33 +0100

    Comienzo Taller #2, Modifico el README y realizo ejercicio para calcular descuentos

--- README.md
+++ README.md
@@ -8,3 +8,11 @@
 - Segundo paso: implementar las fórmulas en JavaScript
 - Tercer paso: crear funciones
 - Cuarto paso: integrar JavaScript con HTML
+
+
+## Taller #2: Porcentajes y Descuentos
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar las fórmulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JavaScript con HTML
---
+++ descuentos.js
@@ -0,0 +1,22 @@
+// let precioOriginal = 100;
+// let descuento = 15;
+
+function calcularPrecioConDescuento(precio, descuento) {
+    let porcentajeDelDescuento = 100 - descuento;
+    let precioConDescuento = precio * porcentajeDelDescuento / 100;
+
+    return precioConDescuento;
+};
+
+function clickPriceDiscount() {
+    let inputPrice = document.getElementById("InputPrice");
+    let inputDiscount = document.getElementById("InputDiscount");
+
+    let valorPrice = inputPrice.value;
+    let valorDiscount = inputDiscount.value;
+
+    let resultadoPrecio = calcularPrecioConDescuento(valorPrice, valorDiscount);
+
+    let resultPrice = document.getElementById("ResultPrice");
+    return resultPrice.innerText = `El precio con descuento es de: ${resultadoPrecio}`
+};
